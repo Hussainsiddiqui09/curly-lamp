17f5e601f396a234d30d8a212be37afff170e62f
Liam Ethan <hg0909029@gmail.com>
2025-07-01 07:25:08 +0500

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,5 @@
+const hamburger = document.getElementById("hamburger");
+const navlinks = document.getElementById("nav-links");
+hamburger.addEventListener("click",()=>{
+    navLinks.classList.toggle("show")
+})
